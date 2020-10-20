d11d22a3c198c2d8af0aef7bdea8d6eb5dce6da4
elad7290 <elad7290@gmail.com>
2020-10-20 19:37:46 +0300

    notice that you had an error but i fix it <3

--- main.cpp
+++ main.cpp
@@ -2,5 +2,5 @@
 void main(){
     std::cout << "hi elad. you didnwt save your changes" << std::endl;
     std::cout<< "i love you more";
-    std:: << "only today. tomorrow is my turn"
+    std:: cout << "only today. tomorrow is my turn";
 }
