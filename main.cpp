414f7e75617df6babda122b45e174ad6449c0a8a
elad7290 <elad7290@gmail.com>
2020-11-03 23:17:52 +0200

    this is the master now

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
-void main(){
+int main(){
     std::cout << "hi elad. you didnwt save your changes" << std::endl;
-    std::cout<< "i love you more";
-    std:: cout << "only today. tomorrow is my turn";
+    std::cout<< "i love you more"<<std::endl;;
+    std:: cout << "only today. tomorrow is my turn"<<std::endl;;
+    std:: cout<<"This is me adding things to my project"<<std::endl;;
 }
